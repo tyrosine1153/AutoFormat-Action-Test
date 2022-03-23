efd111e05f8d5e04476601cc7fcbf114009d679f
Yu Jeongmin <tyrosine1153@gmail.com>
2022-03-23 20:30:11 +0900

    Edit sample scripts to test

--- Example/Assets/Scripts/EventBanner.cs
+++ Example/Assets/Scripts/EventBanner.cs
@@ -1,31 +1,15 @@
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-namespace Nekoyume.UI.Module
-{
-    public class EventBanner : Widget
-    {
-        [SerializeField]
-        private RectTransform content = null;
+using System.Collections.Generic;using UnityEngine;using UnityEngine.UI;
+namespace Nekoyume.UI.Module { public class EventBanner : Widget { [SerializeField]
+private RectTransform content = null;
 
-        [SerializeField] private RectTransform indexContent = null;[SerializeField] private GameObject Banner;[SerializeField]
-        private GameObject IndexOn;[SerializeField] private GameObject IndexOff;[SerializeField] private PageView pageView;
+[SerializeField] private RectTransform indexContent = null; [SerializeField] private GameObject Banner; [SerializeField]
+private GameObject IndexOn; [SerializeField] private GameObject IndexOff; [SerializeField] private PageView pageView;
 
-        private void Awake()
-        {
-            if (EventManager.TryGetArenaSeasonInfo(Game.Game.instance.Agent.BlockIndex, out var info))
-            {
-                var banner = Instantiate(Banner, content);
-                banner.GetComponent<EventBannerItem>().Set(info.SeasonBanner, info.SeasonUrl);
-            }
+private void Awake() { if (EventManager.TryGetArenaSeasonInfo(Game.Game.instance.Agent.BlockIndex, out var info)) { var banner = Instantiate(Banner, content);
+banner.GetComponent<EventBannerItem>().Set(info.SeasonBanner, info.SeasonUrl); }
 
-            for (var i = 0; i < content.childCount; i++)
-            {
-                Instantiate(i == 0 ? IndexOn : IndexOff, indexContent);
-            }
-            var indexImages = new List<Image>();
-            for (var i = 0; i < indexContent.childCount; i++) { indexImages.Add(indexContent.GetChild(i).GetComponent<Image>()); }
-            pageView.Set(content, indexImages);
-        }
-    }
+for (var i = 0; i < content.childCount; i++) { Instantiate(i == 0 ? IndexOn : IndexOff, indexContent);
+} var indexImages = new List<Image>();
+for (var i = 0; i < indexContent.childCount; i++) { indexImages.Add(indexContent.GetChild(i).GetComponent<Image>()); }
+pageView.Set(content, indexImages); } }
 }
